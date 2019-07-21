8015ad9dc15bcfc56c94339f86c1f2441adfb573
Krishnaarunangsu <arunangsutech@gmail.com>
2019-07-21 11:24:29 +0530

    3rd Commit

--- db_connection/python_mongo_client_2.py
+++ db_connection/python_mongo_client_2.py
@@ -8,7 +8,23 @@ from pymongo import MongoClient
 # We can also specify the host and port explicitly, as follows:
 #client = MongoClient('localhost', 27017)
 
+# "mongodb": {
+#                 "mongo_url": "mongodb://172.16.3.1:27017/?replicaSet=rs0",
+#                 "database": "xprdb",
+#                 "mongo_uid": "xprdb_admin",
+#                 "mongo_pwd": "xprdb@Abz00ba",
+#                 "w": 1,
+#                 "interval_between_retries": 5,
+#                 "max_retries": 2
+#         },
 
+DB_USER = 'xprdb_admin'
+DB_PASS = 'xprdb@Abz00ba'
+DB_MACHINE = '172.16.3.1'
+DB_PORT = 27017
+DB_SOURCE = 'xprdb'
+
+# *********************************************
 DB_USER = 'abzAdmin'
 DB_PASS = 'Abz00ba1nc'
 DB_MACHINE = 'localhost'
